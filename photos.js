222b06bb691ec3ecda823b086acce92ab2199025
rotem1chen <rotem1chen@gmail.com>
2026-08-26 22:39:15 +0300

    Add first real STILLS concert: Yuvi @ Reading 3 (33 shots)

--- photos.js
+++ photos.js
@@ -9,10 +9,15 @@
 
 const CONCERTS = [
   {
-    artist: "Sample Concert",
-    venue: "Venue Name",
-    date: "2026",
-    dir: "photos/sample/",
-    shots: ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg"],
+    artist: "Yuvi",
+    venue: "רידינג 3",
+    date: "August 2026",
+    dir: "photos/yuvi/",
+    shots: [
+      "01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg", "06.jpg", "07.jpg", "08.jpg",
+      "09.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg",
+      "17.jpg", "18.jpg", "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg",
+      "25.jpg", "26.jpg", "27.jpg", "28.jpg", "29.jpg", "30.jpg", "31.jpg", "32.jpg", "33.jpg",
+    ],
   },
 ];
